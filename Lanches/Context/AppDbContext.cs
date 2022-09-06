440e6506c20e7768de6035214021a4bd299715cc
Eduardo <eduardotakeuchi@outlook.com>
2022-09-06 15:40:32 -0300

    Created Carrinho de Compras

--- Lanches/Context/AppDbContext.cs
+++ Lanches/Context/AppDbContext.cs
@@ -12,5 +12,6 @@ namespace Lanches.Context
 
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
     }
 }
