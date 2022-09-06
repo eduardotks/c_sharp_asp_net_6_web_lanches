3f32db6b7442e2e024a6f672524a4d3246509450
Eduardo <eduardotakeuchi@outlook.com>
2022-09-06 14:30:41 -0300

    Implementing view model

---
+++ Lanches/ViewModels/LancheListViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Lanches.Models;
+
+namespace Lanches.ViewModels;
+public class LancheListViewModel
+{
+    public IEnumerable<Lanche> Lanches { get; set; }
+    public string CategoriaAtual { get; set; }
+}
