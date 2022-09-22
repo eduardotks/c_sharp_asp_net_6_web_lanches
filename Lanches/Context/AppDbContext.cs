63d12ad5fe601ff86c93ff7a53629a5968d86acf
Eduardo <eduardotakeuchi@outlook.com>
2022-09-22 17:00:56 -0300

    Add repositório de pedidos

--- Lanches/Context/AppDbContext.cs
+++ Lanches/Context/AppDbContext.cs
@@ -7,11 +7,12 @@ namespace Lanches.Context
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
-
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidoDetalhes { get; set; }
     }
 }
