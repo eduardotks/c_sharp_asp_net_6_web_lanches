2ff46a292b6869d6fe60309bf635aa3f3b36a8f8
Eduardo <eduardotakeuchi@outlook.com>
2022-09-22 14:07:32 -0300

    Exibindo lanches a partir de uma view model

--- Lanches/Controllers/HomeController.cs
+++ Lanches/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Lanches.Models;
+using Lanches.Repositories.Interfaces;
+using Lanches.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,10 +8,20 @@ namespace Lanches.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILancheRepository _lancheRepository;
+
+        public HomeController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
+            return View(homeViewModel);
         }
 
 
--- Lanches/Startup.cs
+++ Lanches/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Lanches.Context;
+using Lanches.Models;
 using Lanches.Repositories;
 using Lanches.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -20,8 +21,8 @@ public class Startup
 
         services.AddTransient<ILancheRepository, LancheRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
-
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp)); 
 
         services.AddControllersWithViews();
 
